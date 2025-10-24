07953b71041b685d1c9f7a48b6e27e432e43daea
nicagusdip <nicolasagustindipalma@gmail.com>
2025-10-24 20:49:55 -0300

    deploy vercel

--- gestionInstituto/settings.py
+++ gestionInstituto/settings.py
@@ -5,6 +5,7 @@ Django settings for gestionInstituto project.
 from pathlib import Path
 from django.contrib.messages import constants as message_constants
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,13 +69,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestionInstituto.wsgi.application'
 
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database configuration
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
