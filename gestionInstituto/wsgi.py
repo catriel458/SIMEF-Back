21c8357ad020256913608b1ce4f6225f34f08fda
nicagusdip <nicolasagustindipalma@gmail.com>
2025-10-24 20:14:35 -0300

    wsgy

--- gestionInstituto/wsgi.py
+++ gestionInstituto/wsgi.py
@@ -4,7 +4,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca_digital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionInstituo.settings')
 
 
 application = get_wsgi_application()
